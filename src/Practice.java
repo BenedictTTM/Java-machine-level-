345cdd157009dcb18d266005ef62279e5b4b6f4c
BenedictTTM <palomakut1@gmail.com>
2026-08-17 16:06:42 +0000

    feat: add Practice class with conditional age-based messaging logic

---
+++ src/Practice.java
@@ -0,0 +1,16 @@
+public class Practice {
+   public static void main(String[] args){
+
+    int age = 90;
+
+    if(age > 40){
+        System.out.println("You can marry ");
+    }else  {
+        if(age < 39 ){
+            System.out.println("Not yet bro");
+        }else {
+            System.out.println("Guyy go school ");
+        }
+    }
+    }
+};
