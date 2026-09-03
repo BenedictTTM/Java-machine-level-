0d3643042b0c271dbf4cd995bb885469c8de8e3d
BenedictTTM <palomakut1@gmail.com>
2026-09-03 23:52:11 +0000

    feat: add getLocation endpoint and import necessary Spring web annotations

--- src/main/java/com/example/demo/controller/LocationController.java
+++ src/main/java/com/example/demo/controller/LocationController.java
@@ -1,10 +1,12 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Location;
@@ -23,5 +25,10 @@ public class LocationController {
 @PostMapping
 public Location postUser (@RequestBody Location location){
     return locationService.addLocation(location);
+}
+
+@GetMapping("/{id}")
+public String getLocation (@PathVariable  Long id ){
+    return locationService.userLocation(id);
 }
 }
