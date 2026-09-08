8d4d45c67672e12d4fe9c2e6354fbae95ef8ef71
BenedictTTM <palomakut1@gmail.com>
2026-09-08 23:29:09 +0000

    feat: add GET /tasks endpoint to TaskController using TaskService

--- src/main/java/com/example/demo/controller/TaskController.java
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -1,11 +1,23 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.service.TaskService;
+import com.example.demo.model.Task;
+import java.util.*;
 
 @RestController 
-@RequestMapping()
+@RequestMapping("/tasks")
 public class TaskController {
-    
+   public final TaskService taskService;
+   public TaskController(TaskService taskService){
+     this.taskService = taskService;
+   }
+
+    @GetMapping
+    public List <Task> getAllTask(){
+        return taskService.getAllTasks();
+    }
     
 }
